1069fa457e5122f83bc6734fccd232fc90758268
Belle Lee <letiya66@gmail.com>
2021-01-26 22:25:58 -0800

    Create PhotoGallery

---
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryActivity.java
@@ -0,0 +1,14 @@
+package com.bignerdranch.android.photogallery;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+
+public class PhotoGalleryActivity extends SingleFragmentActivity {
+
+    @Override
+    public Fragment createFragment() {
+        return PhotoGalleryFragment.newInstance();
+    }
+}
